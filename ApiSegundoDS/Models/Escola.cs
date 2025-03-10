803dec2f3b671e1c9bf17aef9d1e78367b7ce88a
Zirtaeb24 <zirtaeb.silva.15@gmail.com>
2025-03-10 09:07:04 -0300

    feat: Classes Escola, Professor e aluno

---
+++ ApiSegundoDS/Models/Escola.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiSegundoDS.Models
+{
+    public class Escola
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+       
+        public Professor Professor { get; set; }
+    }
+}
